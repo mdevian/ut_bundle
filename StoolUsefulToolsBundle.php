8cd24aca42727401e7d3897204fcb7908aabb587
Oleg Emelyanov <oleg.emelyanov@wikimart.ru>
2014-12-19 15:10:24 +0300

    init commit

--- StoolUsefulToolsBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Stool\UsefulToolsBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class StoolUsefulToolsBundle extends Bundle
-{
-}
---
+++ WikimartUsefulToolsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Wikimart\UsefulToolsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WikimartUsefulToolsBundle extends Bundle
+{
+}
